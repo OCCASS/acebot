18a69c2ba7f50fc848602afec13895cc224ae572
Timur Pavlov <72919856+OCCCAS@users.noreply.github.com>
2022-04-14 18:34:31 +0300

    fix: code refacotring

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -8,6 +8,7 @@ from utils.send import send_message
 
 @dp.message_handler(state='*', content_types=types.ContentTypes.ANY)
 async def process_all_messages(message: types.Message, state: FSMContext):
+    state = await state.get_state()
     await send_message(
         'Вы зачем бота ломаете -_-. Не ломайте его пожалуйста, а если что-то сработало некорректно, '
-        f'то отправьте пожалуйста скрин @boost_ace. Current state {state.get_state()}')
+        f'то отправьте пожалуйста скрин @boost_ace. Current state {state}')
