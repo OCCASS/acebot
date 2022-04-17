d5548ccf2786926c07e544da58ae4d8e8635b1b1
Timur Pavlov <72919856+OCCCAS@users.noreply.github.com>
2022-04-17 22:53:17 +0300

    fix: banned user middleware was rewrote

--- middlewares/banned_users.py
+++ middlewares/banned_users.py
@@ -1,12 +1,16 @@
 from aiogram import types
 from aiogram.dispatcher.handler import CancelHandler
 from aiogram.dispatcher.middlewares import BaseMiddleware
-from loader import db
+from service.database.models import Ban, User
 
 
 class BannedUsersMiddleware(BaseMiddleware):
     async def on_process_message(self, message: types.Message, data: dict):
         from_user_id = message.from_user.id
-        if await db.is_user_banned(from_user_id):
-            await message.answer('You are banned', reply_markup=types.ReplyKeyboardRemove())
-            raise CancelHandler()
+        user = await User.query.where(User.telegram_id == from_user_id).gino.first()
+
+        if user:
+            ban = await Ban.query.where(Ban.to_user_id == user.id)
+            if ban:
+                await message.answer('You are banned', reply_markup=types.ReplyKeyboardRemove())
+                raise CancelHandler()
