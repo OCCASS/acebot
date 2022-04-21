18de5e418454a1e618205d7b2fc2ad1f0d92b3e7
Timur Pavlov <72919856+OCCCAS@users.noreply.github.com>
2022-04-21 13:10:56 +0300

    feat: delete keyboard fix

--- utils/delete_keyboard.py
+++ utils/delete_keyboard.py
@@ -1,5 +1,9 @@
 from aiogram import types
+from aiogram.utils.exceptions import MessageCantBeEdited
 
 
 async def delete_keyboard(message: types.Message):
-    await message.edit_reply_markup(None)
+    try:
+        await message.edit_reply_markup(None)
+    except MessageCantBeEdited:
+        pass
