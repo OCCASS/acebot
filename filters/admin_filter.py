b7a691ec87ec56084755ee1c5413c9c560c7721b
Timur Pavlov <72919856+OCCCAS@users.noreply.github.com>
2022-04-24 19:50:47 +0300

    feat: added new function send message to subs

--- filters/admin_filter.py
+++ filters/admin_filter.py
@@ -6,5 +6,8 @@ from data.config import ADMINS
 class IsAdmin(Filter):
     key = "is_admin"
 
+    def __init__(self, is_admin):
+        self.is_admin = is_admin
+
     async def check(self, message: types.Message):
         return message.from_user.id in ADMINS
