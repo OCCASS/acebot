34670fd3e965e5692e260ef1c20beb20128afbcd
Timur Pavlov <72919856+OCCCAS@users.noreply.github.com>
2022-04-25 17:04:43 +0300

    fix: photo_link fix

--- utils/photo_link.py
+++ utils/photo_link.py
@@ -1,19 +1,17 @@
 from io import BytesIO
+from urllib.parse import urljoin
 
-import aiohttp
 from aiogram import types
 
 from loader import bot
 
 
 async def photo_link(photo: types.photo_size.PhotoSize) -> str:
-    with await photo.download(BytesIO()) as file:
-        form = aiohttp.FormData()
-        form.add_field(name='file', value=file)
+    file = await photo.download(destination_file=BytesIO())
+    session = await bot.get_session()
+    async with session.post('https://telegra.ph/upload', data={'file': file}) as response:
+        img_src = await response.json()
+        response.close()
 
-        async with bot.session.post('https://telegra.ph/upload', data=form) as response:
-            img_src = await response.json()
-            response.close()
-
-    link = 'https://telegra.ph/' + img_src[0]["src"]
+    link = urljoin('https://telegra.ph/', img_src[0]["src"])
     return link
