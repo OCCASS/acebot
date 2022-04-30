979797c56933a006e3ad7a6be9dc7270b994875e
Timur Pavlov <72919856+OCCCAS@users.noreply.github.com>
2022-04-30 22:47:17 +0300

    fix: load_bad_words fix

--- utils/load_bad_words.py
+++ utils/load_bad_words.py
@@ -4,9 +4,9 @@ import logging
 def load_bad_words():
     try:
         with open('bad_words', 'r') as file:
-            text = file.read().strip().replace(' ', '')
+            text = file.read().strip()
 
-        return text.split(',')
+        return text.split(', ')
     except (FileNotFoundError, FileExistsError):
         logging.error('File bad_words not found!')
         return []
