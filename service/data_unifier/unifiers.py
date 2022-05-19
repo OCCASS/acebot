7114bded824ac7822ce05dbd5fb7addb817f4077
Timur Pavlov <72919856+OCCCAS@users.noreply.github.com>
2022-05-19 19:43:54 +0300

    fix: data unifier fix

--- service/data_unifier/unifiers.py
+++ service/data_unifier/unifiers.py
@@ -4,21 +4,18 @@ from service.forms import play_level_form
 
 class PeopleIRLDataUnifier(BaseDataUnifier):
     async def fill_description(self):
-        self._data['description'] = '<b>О себе</b>: {about_yourself}\n\n'
-        '<b>Хобби</b>: {hobby}'.format(
-            about_yourself=self._raw_data.pop('about_yourself'),
-            hobby=self._raw_data.pop('hobby')
-        )
+        self._data['description'] = '<b>О себе</b>: {about_yourself}\n\n<b>Хобби</b>: {hobby}'.format(
+            about_yourself=self._raw_data.pop('about_yourself'), hobby=self._raw_data.pop('hobby'))
 
 
 class JustPlayDataUnifier(BaseDataUnifier):
     async def fill_description(self):
         play_level = await play_level_form.get_by_id(self._raw_data.pop('play_level'))
         play_level = play_level.text
-        self._data['description'] = '<b>Что-то о себе</b>: {about_yourself}\n'
-        '<b>К/Д</b>: {call_down}\n'
-        '<b>Уровень игры</b>: {play_level}'.format(
+        self._data[
+            'description'] = '<b>Что-то о себе</b>: {about_yourself}\n' \
+                             '<b>К/Д</b>: {call_down}\n' \
+                             '<b>Уровень игры</b>: {play_level}'.format(
             about_yourself=self._raw_data.pop('about_yourself'),
             call_down=self._raw_data.pop('call_down'),
-            play_level=play_level
-        )
+            play_level=play_level)
